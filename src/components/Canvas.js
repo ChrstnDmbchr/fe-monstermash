25783f72fb9b492d563786a30ddea6646c4ac811
ChrstnDmbchr <christiandambacher@gmail.com>
2018-07-10 09:43:29 +0100

    correct mash load on mount and props chnage now working

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -9,6 +9,7 @@ class Canvas extends Component {
     dragging: false,
     color: "black",
     token: localStorage.getItem('monstermash-id'),
+    currMash: {},
   };
 
   setRadius = (e) => {
@@ -62,38 +63,93 @@ class Canvas extends Component {
     });
   };
 
-  loadCanvas = () => {
+  loadCanvas = (dataUrl) => {
     const canvas = this.refs.canvas;
     const context = canvas.getContext('2d');
 
-    let image = new Image();
-    image.onload = () => {
-      context.drawImage(image, 0, 0);
+    let imageObj = new Image();
+    imageObj.onload = () => {
+      context.drawImage(imageObj, 0, 0);
     };
-    image.src = monsterPart.head;
+    imageObj.src = dataUrl;
   };
 
-  clearCanvas = () => {
+  clearCanvas = (part) => {
     this.refs.canvas.width = this.refs.canvas.width;
     this.refs.canvas.getContext('2d').lineWidth = this.state.radius * 2;
-    this.loadCanvas(monsterPart.head)
+    if (!part) {
+      this.loadCanvas(monsterPart.head)
+    } else {
+      this.loadCanvas(monsterPart[part])
+    }
+  };
+
+  componentWillReceiveProps (newProps) {
+    this.setState({
+      currMash: null
+    }, () => {
+      this.clearCanvas(newProps.match.params.stage)
+    });
+
+    const { token, radius } = this.state;
+
+    const canvas = this.refs.canvas;
+    const context = canvas.getContext('2d');
+    context.lineWidth = radius * 2;
+    canvas.addEventListener('mousemove', this.draw); 
+
+    if (newProps.match.params.stage === 'continue') {
+      fetch('http://localhost:3000/api/mash/continuemash', {
+        headers: {"Authorisation": `Bearer ${token}`}
+      })
+      .then(res => {
+        return res.json();
+      })
+      .then(continueMash => {
+        const { mash } = continueMash
+        this.setState({
+          currMash: mash
+        }, () => {
+          this.loadCanvas(monsterPart[mash.phase]);
+        });
+      })
+      .catch(err => console.log(err))
+    } else {
+      this.loadCanvas(monsterPart.head);
+    };
   };
 
   componentDidMount () {
-    if (!this.state.token) {
-      this.props.history.push('/login')
+    const { token, radius } = this.state;
+
+    if (!token) {
+      this.props.history.push('/login');
     };
-    
+
     const canvas = this.refs.canvas;
     const context = canvas.getContext('2d');
-
-    // sets the lines in between the draw points to the width of the cirlce.
-    context.lineWidth = this.state.radius * 2;
-
+    context.lineWidth = radius * 2;
     canvas.addEventListener('mousemove', this.draw); 
 
-    // invoke function with the dataURL from db to load canvas with placeholder
-    this.loadCanvas(monsterPart.head);
+    if (this.props.match.params.stage === 'continue') {
+      fetch('http://localhost:3000/api/mash/continuemash', {
+        headers: {"Authorisation": `Bearer ${token}`}
+      })
+      .then(res => {
+        return res.json();
+      })
+      .then(continueMash => {
+        const { mash } = continueMash
+        this.setState({
+          currMash: mash
+        }, () => {
+          this.loadCanvas(monsterPart[mash.phase]);
+        });
+      })
+      .catch(err => console.log(err))
+    } else {
+      this.loadCanvas(monsterPart.head);
+    }
   };
 
   render() {
@@ -102,7 +158,7 @@ class Canvas extends Component {
       <div className="canvas">
         <div className="canvas-title">
           <h1 className="title">Time to start a new Monster Mash!</h1>
-          <h1 className="subtitle">you are drawing the {!currMash ? 'head!' : currMash + '!'}</h1>
+          <h1 className="subtitle">you are drawing the {!currMash ? 'head!' : currMash.phase + '!'}</h1>
           <h1 className="subtitle">connect your body part to the marker on the canvas to all the part line up!</h1>
         </div>
         <canvas ref="canvas" className="canvas-area" width="800" height="400" onMouseDown={this.engage} onMouseUp={this.disengage}><strong>Your browser does not cupport canvas</strong></canvas>
@@ -121,7 +177,7 @@ class Canvas extends Component {
             <button onClick={this.userSignUp} className="button nav-button">Submit Mash!</button>
           </div>
           <div className="control">
-            <button className="button" onClick={this.clearCanvas}>Clear Canvas</button>
+            <button className="button" onClick={() => !currMash ? this.clearCanvas : this.clearCanvas(currMash.phase)}>Clear Canvas</button>
           </div>
         </div>
         
