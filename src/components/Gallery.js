0d9bd889321e8f3666333b88fbfa84bff91651d4
ChrstnDmbchr <christiandambacher@gmail.com>
2018-07-11 10:14:37 +0100

    gallery layout complete

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -4,6 +4,7 @@ import '../styles/Gallery.css';
 class Gallery extends Component {
   state = {
     token: localStorage.getItem('monstermash-id'),
+    user: {}
   }
 
   componentDidMount () {
@@ -11,11 +12,47 @@ class Gallery extends Component {
     if (!token) {
       return this.props.history.push('/login');
     };
-  }
-  
+
+    fetch(`http://localhost:3000/api/user`, {
+      headers: {"Authorisation": `Bearer ${token}`}
+    })
+    .then(res => {
+      return res.json();
+    })
+    .then(user =>{
+      this.setState({
+        user
+      });
+    })
+    .catch(err => console.log(err));
+  };
+
   render() {
+    const { user } = this.state;
     return (
-      <div>Gallery</div>
+      <div className="gallery">
+        <h1 className="title">Gallery</h1>
+        <h2 className="subtitle">So {user.username}, Let's see what you've made!</h2>
+        <div className="field is-grouped is-grouped-centered">
+          <div className="control">
+            <button onClick={this.userSignUp} className="button nav-button">Your Monster Mashes!</button>
+          </div>
+          <div className="control">
+            <button onClick={this.goBack} className="button nav-button">Global Gallery</button>
+          </div>
+        </div>
+        <div className="gallery-area">
+          <h1>h1</h1>
+          <h1>h1</h1>
+          <h1>h1</h1>
+          <h1>h1</h1>
+          <h1>h1</h1>
+          <h1>h1</h1>
+          <h1>h1</h1>
+          <h1>h1</h1>
+          <h1>h1</h1>
+        </div>
+      </div>
     )
   };
 };
